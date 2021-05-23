317926a763e9c9526842c67153d05e916b481050
Andrerson Freitas <andrersonn@gmail.com>
2021-05-23 14:38:18 -0300

    API Routes no Next.js/Autenticação com Next Auth

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 
 import Head from 'next/head';
 
@@ -6,6 +6,10 @@ import { SubscribeButton } from '../components/SubscribeButton';
 
 import styles from './home.module.scss';
 import { stripe } from '../services/stripe';
+
+// Client-side
+// Server-side
+// Static Site Generation
 
 interface HomeProps {
   product: {
@@ -39,7 +43,7 @@ export default function Home({ product }: HomeProps ) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
 
   const price = await stripe.prices.retrieve('price_1Iu3TNI1NkndFzKgROgNqTcv')
 
@@ -54,6 +58,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product,
-    }
+    },
+    revalidate: 60 * 60 * 24 // 24 hours
   }
 }
